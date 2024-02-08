470644344dab369b7a9135f6fe06ca138b53e615
badrlah9 <locopedro1507@gmail.com>
2024-02-08 10:18:52 +0100

    Create Products - Save the data
    update controller for saving the products and fields in the dataBase

--- registration-login-articulos/src/main/java/com/example/registrationlogindemo/controller/ProductsController.java
+++ registration-login-articulos/src/main/java/com/example/registrationlogindemo/controller/ProductsController.java
@@ -10,6 +10,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -33,6 +45,54 @@ public class ProductsController {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
         return "products/CreateProduct";
+    }
+
+
+    // new method to create a new products and add it to the product list
+    @PostMapping("/create")
+    public String createProduct(
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result){
+
+        //if the users didn't upload any imageFile
+        if(productDto.getImageFile().isEmpty()){
+            result.addError(new FieldError("productDto", "imageFile", "the image file is required"));
+        }
+        //check if we have any validation error
+        if(result.hasErrors()){
+            return "products/CreateProduct";
+        }
+        //if we don't have any error save the image on the server
+        MultipartFile image = productDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+        try{
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
+            if (!Files.exists(uploadPath)){
+                Files.createDirectories(uploadPath);
+            }
+            try(InputStream inputStream = image.getInputStream()){
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        }catch (Exception ex){
+            System.out.println("Exception: " + ex.getMessage());
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setCreatedAt(createdAt);
+        product.setImageFilename(storageFileName);
+
+        //save product on the database
+        repo.save(product);
+
+        return "redirect:/products";
     }
 
 
